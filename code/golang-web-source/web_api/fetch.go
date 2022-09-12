db0ddc684760c7341dbe76f46e8626abe228a045
joanitolopo <682019013@student.ukws.edu>
2022-09-12 10:49:56 +0700

    fetch

--- code/golang-web-source/web_api/fetch.go
+++ code/golang-web-source/web_api/fetch.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"net/url"
 )
+
+var baseURL = "http://localhost:8080"
 
 type student struct {
 	ID    string
@@ -12,11 +16,9 @@ type student struct {
 	Grade int
 }
 
-var baseURL = "http://localhost:8080"
-
 func fetchUsers() ([]student, error) {
 	var err error
-	var client = &http.Client{} // instance http.Client. Objek yang diperlukan untuk eksekusi request
+	var client = &http.Client{}
 	var data []student
 
 	request, err := http.NewRequest("POST", baseURL+"/users", nil)
@@ -24,11 +26,10 @@ func fetchUsers() ([]student, error) {
 		return nil, err
 	}
 
-	response, err := client.Do(request) // eksekusi request
+	response, err := client.Do(request)
 	if err != nil {
 		return nil, err
 	}
-
 	defer response.Body.Close()
 
 	err = json.NewDecoder(response.Body).Decode(&data)
@@ -39,15 +40,52 @@ func fetchUsers() ([]student, error) {
 	return data, nil
 }
 
+func fetchUser(ID string) (student, error) {
+	var err error
+	var client = &http.Client{}
+	var data student
+
+	var param = url.Values{} // menghasilkan objek yang nantinya digunakan sebagai form data request
+	param.Set("id", ID)
+	var payload = bytes.NewBufferString(param.Encode()) // objek form data di encode lalu diubah menjadi bentuk bytes.Buffer
+
+	request, err := http.NewRequest("POST", baseURL+"/user", payload)
+	if err != nil {
+		return data, err
+	}
+	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	response, err := client.Do(request)
+	if err != nil {
+		return data, err
+	}
+	defer response.Body.Close()
+
+	err = json.NewDecoder(response.Body).Decode(&data)
+	if err != nil {
+		return data, err
+	}
+
+	return data, nil
+}
+
 func main() {
-	var users, err = fetchUsers()
+	// var users, err = fetchUsers()
+	// if err != nil {
+	// 	fmt.Println("Error!", err.Error())
+	// 	return
+	// }
+
+	// for _, each := range users {
+	// 	fmt.Printf("ID: %s\t Name: %s\t Grade: %d\n", each.ID, each.Name, each.Grade)
+	// }
+
+	var user1, err = fetchUser("E001")
 	if err != nil {
 		fmt.Println("Error!", err.Error())
 		return
 	}
 
-	for _, each := range users {
-		fmt.Printf("ID: %s\t Name: %s\t Grade: %d\n", each.ID, each.Name, each.Grade)
-	}
+	fmt.Printf("ID: %s\t Name: %s\t Grade: %d\n", user1.ID, user1.Name, user1.Grade)
 
 }
